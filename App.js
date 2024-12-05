bc7b1d0dd7144d370ca3ff8b7ce1fcd3e30a35df
Saja-Albinali <sajaalbinali176@gmail.com>
2024-12-05 11:20:35 +0300

    ..

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import MainNavigation from "./src/navigation/MainNav/MainNavigation";
+import Stories from "./src/screens/Stories";
 
 export default function App() {
   return (
     <NavigationContainer>
+     
       <MainNavigation />
+{/* <Stories/> */}
 
-      <View>{/* <StatusBar style="auto" /> */}</View>
     </NavigationContainer>
   );
 }
---
+++ src/data/FollowindStories.js
@@ -0,0 +1,52 @@
+const FollowingStories = [
+  {
+    id: 1,
+    followerName: "Sara",
+    followerImage:
+      "https://img.freepik.com/premium-photo/3d-portraits-happy-people-white-background-cartoon-characters-women-girl-vector-illustration_623919-4845.jpg",
+  },
+  {
+    id: 2,
+    followeryName: "Abdula",
+    followerImage:
+      "https://i.pinimg.com/474x/2a/07/36/2a0736b064272c56efdd8b482448964e.jpg",
+  },
+  {
+    id: 3,
+    followerName: "Ali",
+    followerImage:
+      "https://img.freepik.com/premium-photo/3d-portraits-happy-people-white-background-cartoon-characters-boy-man-vector-illustration_623919-4275.jpg",
+  },
+  {
+    id: 4,
+    followerName: "Lulu",
+    followerImage:
+      "https://static.vecteezy.com/system/resources/previews/026/976/736/non_2x/3d-icon-avatar-woman-illustration-of-smiling-happy-girl-cartoon-close-up-portrait-people-of-standing-teenager-on-isolated-on-transparent-background-generative-ai-png.png",
+  },
+  {
+    id: 5,
+    followerName: "Nadia",
+    followerImage:
+      "https://i.pinimg.com/474x/70/a2/36/70a236f90d2803f9da32d0558be75ba1.jpg",
+  },
+  {
+    id: 6,
+    followerName: "Amna",
+    followerImage:
+      "https://static.vecteezy.com/system/resources/previews/026/976/748/non_2x/3d-icon-avatar-woman-illustration-of-smiling-happy-girl-with-phone-cartoon-close-up-people-portrait-of-standing-teenager-on-isolated-on-transparent-background-generative-ai-png.png",
+  },
+  {
+    id: 7,
+    followerName: "Dhai",
+    followerImage:
+      "https://img.freepik.com/premium-photo/3d-portraits-happy-people-white-background-cartoon-characters-women-girl-vector-illustration_623919-4807.jpg",
+  },
+  {
+    id: 8,
+    followerName: "Rashed",
+    followerImage:
+      "https://www.aiscribbles.com/img/variant/large-preview/27014/?v=72f1fb",
+  },
+];
+
+export default FollowingStories;
